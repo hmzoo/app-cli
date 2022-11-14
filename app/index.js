d88798f28f0c4ab89506fff289a569d43351fa37
Ubuntu <ubuntu@sarthe.fr>
2022-11-14 11:41:29 +0100

    ok

--- app/index.js
+++ app/index.js
@@ -1,31 +1,95 @@
 const express = require('express');
-const conf = new (require('conf'))()
+const Redis = require("ioredis");
+const { v4: uuidv4 } = require('uuid');
+
+
+const redis = new Redis({
+  host: 'localhost',
+  port: 6379,
+});
+
 
 const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({
-      message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-    });
-  });
 
-  app.get('/set', (req, res) => {
-    if(req.query.k && req.query.v){
-      conf.set(req.query.k,req.query.v);
+  res.json({
+    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+  });
+});
+
+app.get('/set', (req, res) => {
+
+  redis.exists(req.query.k).then(ans => {
+    console.log("exists", ans)
+
+    if (ans == 0) {
+      if (req.query.uid) {
+        return redis.set(req.query.k, req.query.v).then(ans => {
+          redis.set(req.query.uid, req.query.k)
+          return res.json({
+            ans: ans
+          });
+        })
+      } else {
+        return res.json({
+          ans: "nouid"
+        })
+      }
+
+    } else {
+      if (!req.query.uid) {
+      return res.json({
+        ans: "notfree"
+      })
+    }else{
+
+      return redis.get(req.query.uid).then(ans => {
+        console.log(req.query.k,ans)
+        if(ans == req.query.k){
+        return redis.set(req.query.k, req.query.k).then(ans=>{
+        return res.json({
+          ans: ans
+        });
+      })}else{
+        return res.json({
+          ans: "notfree"
+        })
+      }
+
+
+    })
+      
+
+
     }
-    
-    res.json({
-      k: req.query.k,v: req.query.v
-    });
-  });
 
-  app.get('/get', (req, res) => {
-    
+    }
+  }).catch(err => {
     res.json({
-      k: req.query.k,v: conf.get(req.query.k)
+      err: err
     });
-  });
- 
- 
+  })
+
+
+});
+
+
+
+app.get('/get', (req, res) => {
+
+  redis.get(req.query.k).then(ans => {
+    return res.json({
+      v: ans
+    });
+  }).catch(err => {
+    res.json({
+      err: err
+    });
+  })
+
+});
+
+
 module.exports = app;
